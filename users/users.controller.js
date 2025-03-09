53a0dae111cee92a7ced4616a2ac373182ea7eb8
MGB7 IO Heart Rizarri <mariannemaecalderon04@gmail.com>
2025-03-10 01:52:11 +0800

    second commit to API_TYPEORM

--- users/users.controller.js
+++ users/users.controller.js
@@ -54,7 +54,7 @@ function createSchema(req, res, next) {
         title: Joi.string().required(),
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
-        role: Joi.string().valid(Role.Admin, Role,User).required(),
+        role: Joi.string().valid(Role.Admin, Role.User).required(),
         email: Joi.string().required(),
         password: Joi.string().min(6).required(),
         confirmPassword: Joi.string().valid(Joi.ref('password')).required()
@@ -67,7 +67,7 @@ function updateSchema(req, res, next) {
         title: Joi.string().empty(''),
         firstName: Joi.string().empty(''),
         lastName: Joi.string().empty(''),
-        role: Joi.string().valid(Role.Admin, Role,User).empty(''),
+        role: Joi.string().valid(Role.Admin, Role.User).empty(''),
         email: Joi.string().empty(''),
         password: Joi.string().min(6).empty(''),
         confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
